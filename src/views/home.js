516ed66aec383ddb8949b60aa0020be929ebc903
Bailey Franklin <baileydfranklin@gmail.com>
2024-01-27 19:31:17 -0700

    Added some Cards to the home page

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,39 @@
-
+import { Card } from "react-bootstrap";
+// import { useState, useEffect } from "react";
 
 
 
 export default function Home() {
+    const cardStyle = {
+        width: "300px", // Adjust the width of the cards as needed
+        height: "300px",
+        border: 'solid black 3px',
+        backgroundColor: "white",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    };
+
+    const containerStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        paddingTop: '10%',
+        paddingLeft: '20%',
+        paddingRight: '20%' // Optional padding between cards
+    };
+
     return (
-        <h1>Hello from the Home Page!</h1>
-    )
+        <div style={containerStyle}>
+            <Card style={cardStyle}>
+                {/* Content for Card 1 */}
+            </Card>
+            <Card style={cardStyle}>
+                {/* Content for Card 2 */}
+            </Card>
+            <Card style={cardStyle}>
+                {/* Content for Card 3 */}
+            </Card>
+        </div>
+    );
 }
